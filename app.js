7d487039bd1ea79c5311c16e97294ca45773f334
Nana Adjei Manu <claeusdev@gmail.com>
2018-08-01 19:33:59 +0000

    add error catching

--- app.js
+++ app.js
@@ -19,7 +19,14 @@ request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
     json: true
 },(error, response, body) => {
-    console.log(`Address: ${body.results[0].formatted_address}`)
-    console.log(`Address: ${body.results[0].geometry.location.lat}`)
-    console.log(`Address: ${body.results[0].geometry.location.lng}`)
+    if (error) {
+        console.log('not working well')
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('unable to find that address')
+    } else if (body.status === 'OK') {
+        console.log(`Address: ${body.results[0].formatted_address}`)
+        console.log(`Address: ${body.results[0].geometry.location.lat}`)
+        console.log(`Address: ${body.results[0].geometry.location.lng}`)
+    }
+    
 })
